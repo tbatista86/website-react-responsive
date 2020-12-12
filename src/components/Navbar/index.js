6e129d2ff43c55e63393433891fd032944436b31
Tiago Batista <tiagobatista86@gmail.com>
2020-12-12 17:35:02 -0300

    create navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
+import { Button } from '../../globalStyle'
+import { MobileIcon, Nav, NavbarContainer, NavBtnLink, NavIcon, NavItem, NavItemBtn, NavLinks, NavLogo, NavMenu } from './NavbarElements'
+
+const Navbar = () => {
+  const [click, setClick] = useState(false)
+  const [button, setButton] = useState(false);
+
+  const handleClick = () => setClick(!click)
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  useEffect(() => {
+    showButton()
+  }, [])
+
+  window.addEventListener('resize', showButton)
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              ULTRA
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/products">Products</NavLinks>
+              </NavItem>
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to="/sign-up">
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to="/sign-up">
+                    <Button fontBig primary>SIGN UP</Button>
+                  </NavBtnLink>
+                )} 
+              </NavItemBtn>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  )
+}
+
+export default Navbar
+
