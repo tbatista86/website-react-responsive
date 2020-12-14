3aeba7561d1aa9a85501a6d428f1f35f04020f0a
Tiago Batista <tiagobatista86@gmail.com>
2020-12-14 16:51:13 -0300

    add more info section

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { InfoSection } from '../../components'
-import { homeObjOne } from './Data'
+import { homeObjFour, homeObjOne, homeObjThree, homeObjTwo } from './Data'
 
 const Home = () => {
   return (
     <>
       <InfoSection {...homeObjOne}/> 
+      <InfoSection {...homeObjTwo}/> 
+      <InfoSection {...homeObjThree}/> 
+      <InfoSection {...homeObjFour}/> 
     </>
   )
 }
